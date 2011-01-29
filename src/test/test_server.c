e61eb7a7302983aeacd3eb07fd47fe1fb7655eea
Jared MacLeod <jmacleod@jmacleod-laptop.(none)>
2011-01-28 23:56:16 -0500

    almost working, but segfaults, I think because I need to separate mutexes

--- src/test/test_server.c
+++ src/test/test_server.c
@@ -16,24 +16,33 @@
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
 #define BACKLOG 10     // how many pending connections queue will hold
 #define NUM_SMTP_LISTENER_THREADS 10 
-pthread_mutex_t thread_ready_mutex     = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t thread_ready_mutex     = PTHREAD_MUTEX_INITIALIZER; //do we maybe need an arry of these for each thread? and a seperate mutex in the fuctions for updating?
 pthread_cond_t  thread_ready_var[NUM_SMTP_LISTENER_THREADS];
 
 void initialize_thread_ready_var (){
     int i;
     for (i=0;i<NUM_SMTP_LISTENER_THREADS;i++){
-        thread_ready_var[i]   = PTHREAD_COND_INITIALIZER;
+        pthread_cond_init(&thread_ready_var[i],NULL);
+        //thread_ready_var[i]   = PTHREAD_COND_INITIALIZER;
     }
 }
 int fds[NUM_SMTP_LISTENER_THREADS];
 
-struct ready_thread_struct {
-   int num;
+//preallocate because we know how many we only need as many as there are threads, that way we don't need to malloc for linked list
+struct  ready_thread_struct {
+   int thread_num;
    struct ready_thread_struct * next;
-};
-typedef struct ready_thread_struct ready_thread_struct;
-ready_thread_struct ready_threads = NULL;
+} ready_thread_struct_array[NUM_SMTP_LISTENER_THREADS];
+struct ready_thread_struct *ready_threads = NULL, *last_ready_thread = NULL;
 
+void initialize_ready_thread_struct(){
+    int i;
+    for (i=0;i<NUM_SMTP_LISTENER_THREADS;i++){
+        ready_thread_struct_array[i].thread_num=i;
+        ready_thread_struct_array[i].next=NULL;
+    }
+}
+    
  
 void sigchld_handler(int s)
 {
@@ -50,37 +59,43 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
  
-int mark_self_as_ready(){
-    //get same mutex as get_next_ready_thread
-    //how does this thread know its number?
-    //will block and wait for cond var, not in here, but after this function returns
-    //need a pointer to the end of the list, so we don;t need to traverse to add threeads to end of the list
-    //malloc a new struct
-    //how does a thread know its number?  howeber that is set num to this thread
-    //set next to null
-    //update last pointer to point to this new cell
-    //if initial list pointer is null, point it to this
-    //point last entries  next to this new struct, it should have been null, if not then error
-    //release mutex
+int mark_self_as_ready(int thread_num){  //maybe this can be the initial state or action in state machine 
 
+    // Lock mutex and then wait for signal to relase mutex
+    pthread_mutex_lock( &thread_ready_mutex );
+    //update self as ready clear 
+    ready_thread_struct_array[thread_num].next = NULL;
+    if (ready_threads->next == NULL){
+        //if the list is empty make this the first node.
+        ready_threads->next = &ready_thread_struct_array[thread_num];
+    }else {
+        //elese insert this at the end
+        last_ready_thread->next = &ready_thread_struct_array[thread_num];
+    }
+    last_ready_thread = &ready_thread_struct_array[thread_num];
+    // mutex unlocked if condition var is set 
+    pthread_cond_wait( &thread_ready_var[thread_num], &thread_ready_mutex );
 }
 
 int get_next_ready_thread() {
     int thread_num;
-    //need to mutex this
+    
+    pthread_mutex_lock( &thread_ready_mutex );
+
     if (ready_threads == NULL){
-        //maybe wait for some time before and try again befoer returning failure, would need to release mutex so threads could be add to list
+        //maybe wait for some time before and try again before returning failure, would need to release mutex so threads could be add to list
         return -1;
     }
-    thread_num = ready_threads->num;
+    thread_num = ready_threads->thread_num;
     ready_threads = ready_threads->next;
-    //delete this ready_thread so we dont leak memory, we have the num, and have updated the pointer
-    //release mutex
+
+    pthread_mutex_unlock( &thread_ready_mutex );
     return(thread_num);
 } 
 
 void *listener (){
-    int sockfd, new_fd;  // listen on sock_fd 
+    int sockfd, new_fd;  // listen on sock_fd, and on accept get a new_fd
+    int ready_thread_num;
     struct addrinfo hints, *servinfo, *p;
     struct sockaddr_storage their_addr; // connector's address information
     socklen_t sin_size;
@@ -162,52 +177,66 @@ void *listener (){
         }
         fds[ready_thread_num] = new_fd; // update ready thread's fd with new one from accept
         // signal that thread
-        pthread_cond_signal( thread_ready_var[ready_thread_num] );
+        pthread_cond_signal( &thread_ready_var[ready_thread_num] );
         pthread_mutex_unlock( &thread_ready_mutex );
     }
 } 
 
-void *handler(){
+void *worker(int *thread_num_in){
+    int thread_num = *thread_num_in;
     int numbytes = 0;
     char buf[MAXDATASIZE];
-
+    printf("entering worker thread_num(%d)\n",thread_num);
     while (1){
-    // Lock mutex and then wait for signal to relase mutex
-    pthread_mutex_lock( &thread_ready_mutex );
-    //update self as ready
-    // mutex unlocked if condition var is set 
-    pthread_cond_wait( &thread_ready_var[0], &thread_ready_mutex );
-    if (send(new_fd, "220 mx.changeme.com ESMTP\n", 26, 0) == -1){
+        mark_self_as_ready(thread_num);
 
-        perror("send");
-    }
+        sprintf(buf,"220 mx.changeme.com ESMTP(thread id: %d\n",thread_num);
+        if (send(fds[thread_num], buf, sizeof(buf), 0) == -1){
+            perror("send");
+        }
 
-    if ((numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1) {
-        perror("recv");
-        exit(1);
-    }
+        if ((numbytes = recv(fds[thread_num], buf, MAXDATASIZE-1, 0)) == -1) {
+            perror("recv");
+            exit(1);
+        }
 
-    buf[numbytes] = '\0';
+        buf[numbytes] = '\0';
 
-    printf("got:  '%s'\n",buf);
+        printf("got:  '%s'\n",buf);
 
-    close(new_fd);
-    pthread_mutex_unlock( &thread_ready_mutex );
-
+        close(fds[thread_num]);
+        pthread_mutex_unlock( &thread_ready_mutex );
   }
 
 }
  
 int main(void)
 {
+    pthread_t listener_thread;
+    pthread_t worker_threads[NUM_SMTP_LISTENER_THREADS];
+    int i;
+
+printf("1\n");
     initialize_thread_ready_var();
-    pthread_t listener_thread, handler_thread;
+printf("2\n");
+    initialize_ready_thread_struct();
 	 
+printf("3\n");
+    for (i=0;i<NUM_SMTP_LISTENER_THREADS;i++){
+        printf("3 %d\n",i);
+        pthread_create( &worker_threads[i], NULL, &worker, &i);
+    }
+
+printf("4\n");
     pthread_create( &listener_thread, NULL, &listener, NULL);
-    pthread_create( &handler_thread, NULL, &handler, NULL);
+
+printf("5\n");
     
     pthread_join( listener_thread, NULL);
-    pthread_join( handler_thread, NULL);
+
+    for (i=0;i<NUM_SMTP_LISTENER_THREADS;i++){
+        pthread_join( worker_threads[i], NULL);
+    }
     return 0;
 }
  
